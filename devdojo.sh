104248f3c79b10feec05bffe54e15a2b82b2d30c
RhithuM <149837337+RhithuM@users.noreply.github.com>
2023-11-14 00:55:11 +0000

    added bash arguments

--- devdojo.sh
+++ devdojo.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-# Ask the user for their name
-
-read -p "What is your name? " name
-
-# Greet the user
-echo "Hi there $name"
-echo "Welcome to DevDojo!"
+./devdojo.com your_argument
+echo "Argument one is $1" $2
