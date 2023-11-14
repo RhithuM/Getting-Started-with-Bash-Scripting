230c2afcdda809b86fe65ca99d222279e28b38ef
RhithuM <149837337+RhithuM@users.noreply.github.com>
2023-11-14 05:23:15 +0000

    added bash fuctions

--- arguments.sh
+++ arguments.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "All arguments: $@"
+#######################################
+# Description: Hello function
+# Globals:
+#   None
+# Arguments:
+#   Single input argument
+# Outputs:
+#   Value of input argument
+# Returns:
+#   0 if successful, non-zero on error.
+#######################################
+function hello() {
+    echo "Hello $1!"
+}
